52bb9a7760d7e5caacbd11110877fbb6caa42fa6
Mateus <ma@gmail.com>
2014-10-20 18:55:26 -0200

    DOM d1

--- DesafioDOM/desafios-dom/desafio_1/desafio_1.js
+++ DesafioDOM/desafios-dom/desafio_1/desafio_1.js
@@ -1,3 +1,15 @@
 addListener(window, "load", function() {
 	//codigo aqui
+	var infoDiv = document.getElementById("info");
+	var div = document.createElement("div");
+	var answer = "";
+	
+	answer = document.getElementsByTagName("blockquote")[0].getAttribute("cite");
+	console.log(answer);
+	//var elem = document.getElementsByTagName("blockquote");
+	
+	
+	
+	
+	//div.appendChild(document.createTextNode(answer));
 });
