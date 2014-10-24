1dcdd3c7509cbbf4a200f1656e46423e2130f0bb
Mateus <ma@gmail.com>
2014-10-24 16:58:07 -0200

    desafioDOM 2 e 3 done

--- DesafioDOM/desafios-dom/desafio_2/desafio_2.js
+++ DesafioDOM/desafios-dom/desafio_2/desafio_2.js
@@ -21,4 +21,55 @@ function cancelDefault(e) {
 
 addListener(window, "load", function() {
     //codigo aqui
-});
+	//Tabs
+	var elem = document.getElementById("tabs").children;
+	elem[0].getElementsByTagName("a")[0].addEventListener("click",Task.handlerTab);
+	elem[1].getElementsByTagName("a")[0].addEventListener('click',Task.handlerTab);
+	elem[2].getElementsByTagName("a")[0].addEventListener('click',Task.handlerTab);
+	
+	//ColorTest
+	elem = document.getElementById("info").lastElementChild.children;
+	elem[0].getElementsByTagName("a")[0].addEventListener("click",Task.handlerColorTest);
+	elem[1].getElementsByTagName("a")[0].addEventListener('click',Task.handlerColorTest);
+	elem[2].getElementsByTagName("a")[0].addEventListener('click',Task.handlerColorTest);
+});		
+
+var Task = {
+	handlerTab: function(e){
+	
+		var tabs = e.currentTarget.parentNode.parentNode;
+		for(p=tabs.firstElementChild;p!=null;p=p.nextElementSibling){
+			p.setAttribute("class","");
+		}
+		e.currentTarget.parentNode.setAttribute("class","selected");
+		
+		var content = document.getElementById("content");
+		for(p=content.firstElementChild;p!=null;p=p.nextElementSibling){
+			p.setAttribute("class","sheet"); 
+		}
+		var tab = e.currentTarget.innerHTML;
+		//console.log(tab);
+		switch(tab){
+			case "Informações":
+				content.children[0].setAttribute("class","intop");
+				break;
+			case "Correções":
+				content.children[1].setAttribute("class","intop");
+				break;
+			case "Atualizações":
+				content.children[2].setAttribute("class","intop");
+				break;
+			default:
+				break;
+		}
+	},
+	
+	handlerColorTest: function(e){
+		var colortest = document.getElementById("colortest");
+		colortest.setAttribute("class",e.currentTarget.innerHTML);
+		window.setTimeout(function() {
+			colortest.setAttribute("class","");;
+		}, 500);
+		//console.log(e.currentTarget.innerHTML);
+	}
+};
--- DesafioDOM/desafios-dom/desafio_3/desafio_3.js
+++ DesafioDOM/desafios-dom/desafio_3/desafio_3.js
@@ -32,4 +32,57 @@ function cancelDefault(e) {
 
 addListener(window, "load", function() {
     //codigo aqui
+	
+	var elem = document.getElementById("control").children;
+	elem[0].addEventListener("click",Task.handlerButtonAdd);
+	elem[1].addEventListener('click',Task.handlerButtonRemove);
+	
+	document.getElementById("pool").firstElementChild.addEventListener("click",Task.handlerThing);
 });
+
+var qtdPool = 0;
+var colors = ["orange","red","blue","silver","green","brown","yellow","pink"];
+
+var Task = {
+	handlerButtonAdd: function(e){
+		
+		var color = document.createElement("button");
+		color.appendChild(document.createTextNode("color"));
+		
+		var remover = document.createElement("a");
+		remover.appendChild(document.createTextNode("remover"));
+		remover.setAttribute("href","#");
+		
+		var div = document.createElement("div");
+		div.setAttribute("class","thing");
+		div.addEventListener("click",Task.handlerThing);
+		
+		div.appendChild(color);
+		div.appendChild(remover);
+		document.getElementById("pool").appendChild(div);
+		
+		qtdPool += 1;
+		document.getElementById("info").innerHTML = "A piscina contém " + qtdPool + " coisas";
+	},
+	
+	handlerButtonRemove: function(e){
+		
+		var pool = document.getElementById("pool");
+		while (pool.firstChild) {
+			pool.removeChild(pool.firstChild);
+		}
+	},
+	
+	handlerThing: function(e){
+		
+		//console.log(e.srcElement);
+		if(e.target.localName === "button"){
+			var op = Math.floor(Math.random() * 7);
+			var c = "background-color:" + colors[op];
+			e.srcElement.setAttribute("style",c);
+		}else if(e.target.localName === "a"){
+			var div = e.currentTarget;
+			div.parentNode.removeChild(div);
+		}
+	}
+};
